cf433a0f21c55a2d4e362d82e8e944ac63a459a6
claritza <claritzapplata@gmail.com>
2025-08-21 10:06:30 -0600

    update

--- Laboratorio 3.R
+++ Laboratorio 3.R
@@ -26,6 +26,75 @@ boxplot(info$altura) #col sirve para colorear la grafica col ="Indianred", # mai
          
 colores = c("navajowhite", "indianred", "skyblue" )
 boxplot(Temp[,2:13], col=colores) 
+
+url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv"
+inspeccion <- read.csv(url)
+
+
+head(inspeccion)
+
+prof_url_2 <- paste0("https://repodatos.atdt.gob.mx/api_update/senasica/",
+                     "actividades_inspeccion_movilizacion/",
+                     "29_actividades-inspeccion-movilizacion.csv")
+senasica <- read.csv(prof_url_2)
+head(senasica)
+
+library(repmis)
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+
+head(conjunto)
+
+library(readr)
+
+file <- paste0("https://raw.githubusercontent.com/mgtagle/",
+               "202_Analisis_Estadistico_2020/master/cuadro1.csv")
+inventario <- read_csv(file)
+
+head(inventario)
+
+dbh<-c(16.5,25.3,22.1,17.2,16.1,8.1,34.3,5.4,5.7,11.2,24.1,
+       14.5,7.7,15.6,15.9,10,17.5,20.5,7.8,27.3,
+       9.7,6.5,23.4,8.2,28.5,10.4,11.5,14.3,17.2,16.8)
+trees <- seq(1,30)
+parcelas <- gl(1,30) 
+
+trees <- data.frame(trees,dbh,parcelas) 
+mean(trees$dbh)
+sd(trees$dbh)
+
+sum(trees$dbh < 10)
+which(trees$dbh < 10)
+trees.13<-trees[!(trees$parcela=="2"),]
+trees.13 
+trees.1<-subset(trees,dbh<=10)
+head(trees.1)
+mean(trees$dbh)
+mean(trees.1$dbh)
+
+# #histogramas -------------------------------------------------------------
+hist(trees$dbh) 
+hist(trees.1$dbh)
+
+mamifero <- read.csv("https://www.openintro.org/data/csv/mammals.csv") 
+hist(mamifero$total_sleep) 
+ 
+hist(mamifero$total_sleep, # Datos
+     xlim = c(0,20), ylim = c(0,14), # Cambiar los límites de x & y
+     main = "Total de horas sueño de las 39 especies", # Cambiar el título
+     xlab = "Horas sueño", # Cambiar eje de las x
+     ylab = "Frecuencia", # Cambiar eje de las y
+     las = 1, # Cambiar orientación de y
+     col = "navajowhite") # Cambiar color de las barras 
+data("chickwts")
+head(chickwts[c(1:2,42:43, 62:64), ]) 
+
+feeds <- table(chickwts$feed)
+feeds 
+
+barplot(feeds) 
+barplot(feeds[order(feeds, decreasing = TRUE)]) 
+
+
 # Estadisticas descriptivas -----------------------------------------------
 
 
